4f425b48000c2eb3544bd94476262b7847c371ae
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2025-02-25 08:09:01 +0100

    playground

--- playground.cpp
+++ playground.cpp
@@ -5,7 +5,9 @@ int main()
 
     std::cout << "Calculator" << std::endl;
     int a = 0, b = 0, c = 0;
-    std::cout << b << std::endl;
+    std::cout << "Enter an integer: ";
+    std::cin >> a;
+    std::cout << "You entered: " << a << std::endl;
 
     return 0;
 }
