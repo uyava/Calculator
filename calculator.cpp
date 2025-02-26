527a189f1e41b0312040594eda6779bf13d76738
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2025-02-26 08:58:20 +0100

    calculator works

--- calculator.cpp
+++ calculator.cpp
@@ -13,17 +13,48 @@ int main()
     // c = a + b;
     // std::cout << a << " + " << b << " = " << c << std::endl;
 
-    std::cout << "Calculator for dividing integers" << std::endl;
-    std::cout << "------------------------------\n" << std::endl;
-    int divident = 0, divisor = 0, quatient = 0, remainder = 0;
+    // std::cout << "Calculator for dividing integers" << std::endl;
+    // std::cout << "------------------------------\n" << std::endl;
+    // int divident = 0, divisor = 0, quatient = 0, remainder = 0;
+    // std::cout << "Enter the first integer: ";
+    // std::cin >> divident;
+    // std::cout << "Enter the second integer: ";
+    // std::cin >> divisor;
+    // quatient = divident / divisor;
+    // remainder = divident % divisor;
+    // std::cout << divident << " / " << divisor << " = " << quatient 
+    // << " (remainder " << remainder << " )" << std::endl;
+
+    std::cout << "Calculator" << std::endl;
+    std::cout << "----------\n" << std::endl;
+    int first = 0, second = 0, result = 0;
+    char calc;
+
     std::cout << "Enter the first integer: ";
-    std::cin >> divident;
+    std::cin >> first;
+    std::cout << "Enter the operator (+,-,*,/): ";
+    std::cin >> calc;
     std::cout << "Enter the second integer: ";
-    std::cin >> divisor;
-    quatient = divident / divisor;
-    remainder = divident % divisor;
-    std::cout << divident << " / " << divisor << " = " << quatient 
-    << " (remainder " << remainder << " )" << std::endl;
+    std::cin >> second;
+
+    if (calc == '+')
+    {
+        result = first + second;
+    }
+    else if (calc == '-')
+    {
+        result = first - second;
+    }
+    else if (calc == '*')
+    {
+        result = first * second;
+    }
+    else if (calc == '/')
+    {
+        result = first / second;
+    }
+
+    std::cout << first << " " << calc << " " << second <<  " = " << result << std::endl;
 
 
     return 0;
